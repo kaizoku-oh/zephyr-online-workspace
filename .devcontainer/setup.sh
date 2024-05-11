27f6e8f7df2933ac61501eabc1062c16f26f8559
Bayrem Gharsellaoui <garssallaoui.bayrem@gmail.com>
2024-05-11 21:33:27 +0000

    Added west manifest

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,19 +1,17 @@
 # Initialize the west repository in the 'app' subdirectory
-# west init -l app
+west init -l app
 
 # Make sure the workspace contains Git repositories matching the projects in the manifest file
-# west update
+west update
 
 # Export Zephyr-specific build system metadata to the 'app' directory
-# west zephyr-export
+west zephyr-export
 
 # Install Python dependencies required by Zephyr
-# pip install -r deps/zephyr/scripts/requirements.txt
+pip install -r deps/zephyr/scripts/requirements.txt
 
 # Install Python dependencies required by MCUBoot
-# pip install -r deps/bootloader/mcuboot/scripts/requirements.txt
+pip install -r deps/bootloader/mcuboot/scripts/requirements.txt
 
 # Copy .vscode to outer directory
 # cp -r app/.vscode .
-
-echo hellooooooooo
